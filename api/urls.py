8a4ca2a7ee21ec64a3e45f9b24fb913ee9502b45
himanshugupta0158 <himanshugupta0158@gmail.com>
2021-11-25 17:14:42 +0530

    added put and patch

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path , include
-from .views import CommentListApiView
+from .views import CommentListApiView , CommentDetailApiView
 urlpatterns = [
     path('', CommentListApiView.as_view()),
+    path('<int:pk>', CommentDetailApiView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +15,6 @@ from api.serializers import CommentSerializer
 
 class CommentListApiView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    queryset = Comment.objects.all()
     
     def get(self, request):
         serializer = CommentSerializer(Comment.objects.all() ,many = True)
@@ -26,4 +27,45 @@ class CommentListApiView(APIView):
             return Response(serializer.data , status = status.HTTP_201_CREATED)
         return Response(serializer.errors , status = status.HTTP_400_BAD_REQUEST)
 
+
+
+class CommentDetailApiView(APIView):
+    # permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self , pk):
+        try:
+            return Comment.objects.get(pk = pk)
+        except :
+            return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    def get(self,request , pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    
+    def put(self , request , pk):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors , status = status.HTTP_400_BAD_REQUEST)
+        
+    def patch(self, request , pk):
+        comment = self.get_object(pk)
+        data = request.data
+        
+        comment.msg = data.get('msg' , comment.msg)
+        comment.user = User.objects.get(id = data.get('user')) if data.get('user') else comment.user
+        comment.created_on = data.get('created_on' , comment.created_on)
+        
+        comment.save()
+        serializer = CommentSerializer(comment)
+        
+        return Response(serializer.data)
+        
+        
+        
+        
+        
         
